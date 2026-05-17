a1f1b60610d204754773a8553ee93a061428df27
Kei <2201103742@student.buksu.edu.ph>
2026-05-17 15:36:54 +0800

    feat: add cache configuration file with database default store support

--- config/cache.php
+++ config/cache.php
@@ -41,9 +41,9 @@ return [
         'database' => [
             'driver' => 'database',
             'connection' => env('DB_CACHE_CONNECTION', 'central'),
-            'table' => env('DB_CACHE_TABLE', 'cache'),
+            'table' => env('DB_CACHE_TABLE', 'public.cache'),
             'lock_connection' => env('DB_CACHE_LOCK_CONNECTION', 'central'),
-            'lock_table' => env('DB_CACHE_LOCK_TABLE'),
+            'lock_table' => env('DB_CACHE_LOCK_TABLE', 'public.cache_locks'),
         ],
 
         'file' => [
